939ca13ebd5f3c5264783623bd14a8a34f12f8df
jboogie <rennabh@gmail.com>
2012-08-04 20:19:08 +0200

    BROKEN: hash funziona ma c'è qualche altro prob (seg fault)

--- hash.c
+++ hash.c
@@ -5,29 +5,21 @@
 #include <string.h>
 
 long int hashf(char * key, int modulo){
-  int i; 
+  int i, hash = 0; 
   unsigned char * hs;
-  char * hash_str, *c;
+  char * hash_str;
 
  hs = (unsigned char *) malloc(MD5_DIGEST_LENGTH);
  hash_str = (char *) malloc(MD5_DIGEST_LENGTH);
- /* hash_str = ""; */
 
- c = (char * ) malloc(sizeof(char));
  MD5((unsigned char *) key, sizeof(char)*strlen(key), hs);
  printf("\nHASH di %s\n", key);
  for( i = 0; i < MD5_DIGEST_LENGTH; i++) {
-   snprintf(c, 1, "%d", hs[i]);
-   hash_str = strcat(hash_str, c);
-   printf("\n%s", hash_str);
+   printf("\n%d", (int) hs[i] % 10);
+   hash = 10*hash + ((int) hs[i] % 10);
  }
- printf("\n FINAL: %s", hash_str);
+ printf("\n FINAL: %d", hash);
 
- /* printf("\n %s \t %s \t %s \t %d\n", key, hs, hash_str, atoi(hash_str)); */
- /* for(i=0; i < strlen(key); ++i){ */
- /*   hash = (hash + (int) key[i]) % modulo; */
- /* } */
-
- return atoi(hash_str);
+ return hash % modulo;
 }
 
