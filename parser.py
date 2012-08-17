83a852989fb1c9b6ff962e65e745a5c84465ceae
jboogie <rennabh@gmail.com>
2012-08-17 18:02:30 +0200

    added books, proceedings and www articles to parser.py

--- parser.py
+++ parser.py
@@ -15,12 +15,20 @@
 import sys
 import re
 
+WORK_INS={'articlein': re.compile(r'<article.*?>'),
+          'bookin': re.compile(r'<book.*?>'),
+          'inproceedingsin': re.compile(r'<inproceedings.*?>'),
+          'wwwin': re.compile(r'<www.*?>')}
+
+WORK_OUTS={'articleout': re.compile(r'</article.*>'),
+          'bookout': re.compile(r'</book.*>'),
+          'inproceedingsout': re.compile(r'</inproceedings.*>'),
+          'wwwout': re.compile(r'</www.*>')}
+
 PATTERNS={'authorin': re.compile(r'<author.*?>'),
           'authorout': re.compile(r'</author.*>'),
           'titlein': re.compile(r'<title.*?>'),
-          'titleout': re.compile(r'</title.*>'),
-          'articlein': re.compile(r'<article.*?>'),
-          'articleout': re.compile(r'</article.*>')}
+          'titleout': re.compile(r'</title.*>')}
 
 def usage():
     print "USAGE: {0} dblp.xml output-file".format(sys.argv[0])
@@ -33,20 +41,21 @@ def parse(inputpath, outputpath):
         print "I/O error({0}): {1}".format(e.errno, e.strerror)
         return
 
-    current_article = [] 
-    is_article = 0
+    current_authors = [] 
+    is_work = 0
     line = inf.readline()
     while line != '':
-        if is_article:
-            #search for  </article>
-            articleout=PATTERNS['articleout'].match(line)
-            if articleout != None:
-                is_article = 0
-                outf.write('\n')
-                while current_article != []:
-                    outf.write(current_article.pop() + '\n')
-            else:
-            	#search for authors:
+        if is_work:
+            #search for ending of WORK block
+            match,workout=0,1
+            for work_out in WORK_OUTS.iteritems():
+                if work_out[1].match(line) != None:
+                    is_work = 0
+                    outf.write('\n')
+                    while current_authors != []:
+                        outf.write(current_authors.pop() + '\n')
+                    break
+            if is_work: #if the previous for loop hasn't matched any closing tag, search for authors
                 start_closing_tag = len(line) - 1
                 for i in range(len(line)-1,0,-1):
                     if line[i] == "<":
@@ -61,8 +70,8 @@ def parse(inputpath, outputpath):
                     authorout=PATTERNS['authorout'].match(line,start_closing_tag)
                     if authorout != None:
                         line = line[:-len(authorout.group(0)) - 1]
-                    	#authors are inserted at the start of current_article
-                        current_article = [line] + current_article 
+                    	#authors are inserted at the start of current_authors
+                        current_authors = [line] + current_authors 
 
                 titlein=PATTERNS['titlein'].match(line)
                 if titlein != None:
@@ -71,16 +80,17 @@ def parse(inputpath, outputpath):
                     titleout=PATTERNS['titleout'].match(line,start_closing_tag)
                     if titleout != None:
                         line = line[:-len(titleout.group(0)) - 1]
-                        #the title is inserted at the end of current_article
-                        current_article = current_article + [line]
+                        #the title is inserted at the end of current_authors
+                        current_authors = current_authors + [line]
 
         elif line == '\n':
             pass
         else:
-            #is the new line <article...> ? if yes set is_article=1
-            articlein=PATTERNS['articlein'].match(line)
-            if articlein != None:
-                is_article = 1
+            #search for starting of WORK block
+            for work_in in WORK_INS.iteritems():
+                if work_in[1].match(line) != None:
+                    is_work = 1
+                    break
         line = inf.readline()        
 
     inf.close()
